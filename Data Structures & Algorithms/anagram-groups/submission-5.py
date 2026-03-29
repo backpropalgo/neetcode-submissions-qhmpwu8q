a7ecad5c79826024133e9ca927adf5927c5da31d
backpropalgo <150304074+backpropalgo@users.noreply.github.com>
2026-03-29 10:23:08 -0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        alpha_counts = [0] * 26
+        word_groups = {}
+        for w in strs:
+            cur_map = [0] * 26
+            for c in w:
+                i = ord(c) - 97
+                cur_map[i] += 1
+            k = '-'.join(str(n) for n in cur_map)
+            print(k)
+            if k in word_groups:
+                word_groups[k].append(w)
+            else:
+                word_groups[k] = [w]
+        return [v for v in word_groups.values()]
+                
+
+       
+        
+        
+            
